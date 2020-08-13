8689b7752d75c66ff93de15cdb3024b769740775
Gustavo <guga.rocha.silveira@hotmail.com>
2020-08-12 22:29:27 -0300

    input slider implemented

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,74 +1,89 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Slider from '@react-native-community/slider';
 
 // Main component
 // Componente princípal
 function Calculator() {
   const [imcValue, setImcValue] = useState(0);
-  const [rating, setRating] = useState('');
-  const [weight, setWeight] = useState('');
-  const [height, setHeight] = useState('');
-  const [colorRating, setColotRating] = useState('#7f8c8d')
+  const [classification, setClassification] = useState('');
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [colorClassification, setColorClassification] = useState('#7f8c8d')
 
+  // function responsible for calculating the imc and defining its classification with the corresponding color
+  // função responsável por calcular o imc e definir sua classificação com a cor correspondente
   function imcCalculation(weight, height) {
     const imc = weight / (height ** 2);
     if (imc < 18.5) {
-      setRating('Magreza');
-      setColotRating('#e74c3c');
+      setClassification('Magreza');
+      setColorClassification('#e74c3c');
     } else if (18.5 <= imc && imc < 25) {
-      setRating('Normal');
-      setColotRating('#2ecc71');
+      setClassification('Normal');
+      setColorClassification('#2ecc71');
     } else if (25 <= imc && imc < 30) {
-      setRating('Sobrepeso');
-      setColotRating('#f1c40f');
+      setClassification('Sobrepeso');
+      setColorClassification('#f1c40f');
     } else if (30 <= imc && imc < 40) {
-      setRating('Obesidade');
-      setColotRating('#f39c12')
+      setClassification('Obesidade');
+      setColorClassification('#f39c12')
     } else if (imc >= 40) {
-      setRating('Obesidade Grave');
-      setColotRating('#e74c3c');
+      setClassification('Obesidade Grave');
+      setColorClassification('#e74c3c');
     }
     setImcValue(imc);
   };
 
   return (
     <View style={styles.container}>
-      <View style={[styles.indicator, {borderColor: colorRating}]}>
-        <Text style={[styles.imcValue, {color: colorRating}]}>
+      <View style={[styles.indicator, {borderColor: colorClassification}]}>
+        <Text style={[styles.imcValue, {color: colorClassification}]}>
           {imcValue.toFixed(1)}
         </Text>
-        <Text style={[styles.rating, {color: colorRating}]}>
-          {rating}
+        <Text style={[styles.classification, {color: colorClassification}]}>
+          {classification}
         </Text>
       </View>
       <View style={styles.inputBlock}>
-        <TextInput
-          style={styles.input}
-          placeholder='Peso (Kg)'
-          keyboardType='number-pad'
-          value={weight}
-          onChangeText={value => {
-            value.replace(',', '.')
-            setWeight(Number(value.replace(',', '.')))
-          }}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder='Altura (m)'
-          keyboardType='number-pad'
-          value={height}
-          onChangeText={value => {
-            setHeight(Number(value.replace(',', '.')))
-          }}
-        />
+
+        <View style={styles.inputContainer}>
+          <View style={styles.inputTextContainer}>
+            <Text style={styles.inputValue}>{weight}</Text>
+            <Text style={styles.inputText}>Kg</Text>
+          </View>
+          <Slider 
+            style={styles.inputSlider}
+            minimumValue={0}
+            maximumValue={200}
+            step={1}
+            onValueChange={value => setWeight(value)}
+            minimumTrackTintColor='#34495e'
+            maximumTrackTintColor='#34495e'
+            thumbTintColor='#34495e'
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <View style={styles.inputTextContainer}>
+            <Text style={styles.inputValue}>{height}</Text>
+            <Text style={styles.inputText}>m</Text>
+          </View>
+          <Slider 
+            style={styles.inputSlider}
+            minimumValue={0}
+            maximumValue={2.5}
+            step={0.01}
+            onValueChange={value => setHeight(value.toFixed(2))}
+            minimumTrackTintColor='#34495e'
+            maximumTrackTintColor='#34495e'
+            thumbTintColor='#34495e'
+          />
+        </View>
+
       </View>
       <TouchableOpacity
         style={styles.button}
-        onPress={() => {
-          imcCalculation(weight, height)
-          setWeight('')
-          setHeight('')
-        }}
+        onPress={() => imcCalculation(weight, height)}
       >
         <Text style={styles.buttonText}>Calcular</Text>
       </TouchableOpacity>
@@ -99,7 +114,7 @@ const styles = StyleSheet.create({
     fontSize: 60,
   },
 
-  rating: {
+  classification: {
     fontSize: 20,
   },
 
@@ -108,13 +123,33 @@ const styles = StyleSheet.create({
     marginVertical: 50
   },
   
-  input: {
-    height: 60,
+  inputContainer: {
+    height: 80,
     marginBottom: 25,
     backgroundColor: '#fff',
     borderRadius: 8,
+    justifyContent: 'flex-end'
+  },
+
+  inputTextContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+
+  inputValue: {
+    fontSize: 35,
+    color: '#bdc3c7',
+    marginRight: 5
+  },
+
+  inputText: {
     fontSize: 24,
-    textAlign: 'center'
+    color: '#bdc3c7',
+  },
+
+  inputSlider: {
+    marginBottom: 5,
   },
 
   button: {
